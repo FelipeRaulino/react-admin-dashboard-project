03c1c7a9524ec40efeb980fd863cff3161cedc1c
FelipeRaulino <felipe.raaulino29@gmail.com>
2023-01-04 06:12:59 -0300

    ajustando configurações com o MongoDB

--- server/index.js
+++ server/index.js
@@ -5,8 +5,10 @@ import helmet from 'helmet';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-
-import { clientRoutes, salesRoutes, managementRoutes, generalRoutes } from './routes';
+import clientRoutes from './routes/client.js';
+import salesRoutes from './routes/sales.js';
+import managementRoutes from './routes/management.js';
+import generalRoutes from './routes/general.js';
 
 /* CONFIGURATION */
 dotenv.config();
@@ -24,3 +26,14 @@ app.use("/client", clientRoutes);
 app.use("/sales", salesRoutes);
 app.use("/management", managementRoutes);
 app.use("/general", generalRoutes);
+
+/* SETTING UP MONGODB */
+const PORT = process.env.PORT || 9000;
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(app.listen(() => console.log(`Server running on PORT: ${PORT}`)
+)).catch(error => console.log(error));
