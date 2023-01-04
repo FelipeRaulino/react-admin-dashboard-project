837a69d2267b250642ac88da6e25d42456831263
FelipeRaulino <felipe.raaulino29@gmail.com>
2023-01-04 14:34:39 -0300

    finalizando configurações de tema

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-import store from "./store";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "./themes";
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-      Hello
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <h1>Hello</h1>
+      </ThemeProvider>
     </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { configureStore } from '@reduxjs/toolkit';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import globalReducer from './state';
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
